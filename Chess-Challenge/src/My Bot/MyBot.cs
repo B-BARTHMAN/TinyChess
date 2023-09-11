bf1cc2325c2c987a4643b09345f30d201917bc9b
B-BARTHMAN <bribri.barth@gmail.com>
2023-09-11 23:20:10 +0200

    plays but really bad

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -9,13 +9,15 @@ public class MyBot : IChessBot
     private int psq_init;
     private readonly int[][] psq_table;
 
+    private Move root_move;
+
     public MyBot() {
 
         psq_table = new []{ 1237940039285380274899124224m, 1237940039285380274899124224m, 1237940039285519912875851776m, 1237940039285380274899124224m, 1237940039285380274899124224m, 1237940039285380274899124224m, 1237940039285519912875851776m, 1237940039285380274899124224m, 
                             1237940039285380274899124351m, 1237940039285380274899124351m, 1237940039285380274899124351m, 1237940039285380274899124438m, 1237940039285380274899124438m, 1237940039285380274899124351m, 1237940039285380274899124351m, 1237940039285380274899124351m, 
                             1237940039285380274899124224m, 1237940039285380274899124224m, 1237940039285380274899124224m, 1237940039285380274899124266m, 1237940039285380274899124266m, 1237940039285380274899124224m, 1237940039285380274899124224m, 1237940039285380274899124224m, 
-                            1237940039285380274899124224m, 1237940039285380274899124224m, 1237940039285380274899124224m, 39304596247310823728047194196m, 39304596247310823728047194196m, 1237940039285380274899124224m, 1237940039285380274899124224m, 1237940039285380274899124224m, 
-                            1237940039285380274899124224m, 1237940039285380274899124224m, 1237940039285380274899124224m, 39304596247310823728047194112m, 39304596247310823728047194112m, 1237940039285380274899124224m, 1237940039285380274899124224m, 1237940039285380274899124224m, 
+                            1237940039285380274899124224m, 1237940039285380274899124224m, 1237940039285380274899124224m, 1237940039285380274899124308m, 1237940039285380274899124308m, 1237940039285380274899124224m, 1237940039285380274899124224m, 1237940039285380274899124224m, 
+                            1237940039285380274899124224m, 1237940039285380274899124224m, 1237940039285380274899124224m, 1237940039285380274899124224m, 1237940039285380274899124224m, 1237940039285380274899124224m, 1237940039285380274899124224m, 1237940039285380274899124224m, 
                             1237940039285380274899124224m, 1237940039285380274899124224m, 1237940039285380274899124224m, 1237940039285380274899124224m, 1237940039285380274899124224m, 1237940039285380274899124224m, 1237940039285380274899124224m, 1237940039285380274899124224m, 
                             1237940039285380274899124224m, 1237940039285380274899124224m, 1237940039285380274899124224m, 1237940039285380274899124224m, 1237940039285380274899124224m, 1237940039285380274899124224m, 1237940039285380274899124224m, 1237940039285380274899124224m, 
                             1237940039285380274899124224m, 1237940039285380274899124224m, 1237940039285380274899124224m, 1237940039285380274899124224m, 1237940039285380274899124224m, 1237940039285380274899124224m, 1237940039285380274899124224m, 1237940039285380274899124224m }
@@ -24,7 +26,7 @@ public class MyBot : IChessBot
                             // LINUS TO DO
                             // PAWN, KNIGHT, BISHOP, ROOK, QUEEN, KING
                             (new []{100, 300, 320, 500, 900, 0, // MG
-                                    200, 400, 450, 700, 1200, 0 // EG
+                                    100, 300, 320, 500, 900, 0 // EG
                             })[psq_init++ % 12]
                             ).ToArray()).ToArray();
         #if DEBUG
@@ -35,38 +37,100 @@ public class MyBot : IChessBot
 
     public Move Think(Board board, Timer timer)
     {
-        Move[] moves = board.GetLegalMoves();
+        Console.WriteLine(search(-999999, 999999, 4, 0));
 
+        int evaluate() {
 
-
-        int Evaluate() {
+            if(board.IsInCheckmate()) return -999999;
+            if(board.IsDraw()) return 0;
 
             int mg_score = 0;
             int eg_score = 0;
-            int gamephase = 42;
+            int gamephase = 62;
 
             for(int color = 0; color < 2; color++){
-                for(int piece_type = 0; piece_type < 12; piece_type++) {
+                for(int piece_type = 0; piece_type < 7; piece_type++) {
 
-                    ulong bb = board.GetPieceBitboard((PieceType)piece_type, color == 0);
+                    ulong bb = board.GetPieceBitboard((PieceType)piece_type, color == 1);
 
                     while(bb != 0){
 
-                        int square = BitboardHelper.ClearAndGetIndexOfLSB(ref bb) ^ 56 * color;
+                        int square = BitboardHelper.ClearAndGetIndexOfLSB(ref bb) ^ 56 * (1 - color);
 
-                        mg_score += psq_table[square][piece_type];
-                        eg_score += psq_table[square][piece_type + 6];
+                        mg_score += psq_table[square][piece_type - 1] * (2 * color - 1);
+                        eg_score += psq_table[square][piece_type + 5] * (2 * color - 1);
 
                         gamephase -= piece_type;
                     }
                 }
             }
 
-            return (gamephase * mg_score + (42 - gamephase) * eg_score) / 42;
+            return (gamephase * eg_score + (62 - gamephase) * mg_score) / 62 * (board.IsWhiteToMove ? 1 : -1);
         }
 
         
-        return moves[0];
+        int search(int alpha, int beta, int depth, int ply){
+            
+            bool qsearch = depth <= 0 && !board.IsInCheck();
+
+            // generate moves
+            Span<Move> moves = stackalloc Move[218];
+            board.GetLegalMovesNonAlloc(ref moves, qsearch);
+
+            // if moves is empty evaluate
+            if(moves.Length == 0 || depth == 0) return evaluate();
+
+            // move ordering
+            int move_value_index = 0;
+            var move_values = new int[218];
+
+            // score moves
+            foreach(Move move in moves){
+                move_values[move_value_index++] = -(
+                
+                // captures
+                move.IsCapture ? 100 * (move.CapturePieceType - move.MovePieceType) + 100 : 
+                
+                //quiet moves
+                0
+
+                );
+            }
+
+            // sort moves
+            move_values.AsSpan(0, move_value_index).Sort(moves);
+
+
+            Move best_move = moves[0];
+            int best_score = -9999999;
+
+            // do moves
+            foreach(Move move in moves) {
+
+                // do recursion
+                board.MakeMove(move);
+                int score = -search(-beta, -alpha, depth - 1, ply + 1);
+                board.UndoMove(move);
+
+                alpha = Math.Max(alpha, score);
+
+                if(score > best_score) {
+                    best_score = score;
+                    best_move = move;
+                    if(ply == 0) root_move = best_move;
+                }
+                // beta cutoff
+                if(alpha >= beta) {
+                    return score;
+                }
+
+            }
+
+            return best_score;
+            
+        }
+
+        return root_move;
     }
 
     
@@ -200,8 +264,8 @@ public class MyBot : IChessBot
                 1, 1, 1, 1, 1, 1, 1, 1,
                 1, 1, 1, 1, 1, 1, 1, 1,
                 1, 1, 1, 1, 1, 1, 1, 1,
-                1, 1, 1, 30, 30, 1, 1, 1,
-                1, 1, 1, 30, 30, 1, 1, 1,
+                1, 1, 1, 1, 1, 1, 1, 1,
+                1, 1, 1, 1, 1, 1, 1, 1,
                 1, 1, 1, 1, 1, 1, 1, 1,
                 1, 1, 1, 1, 1, 1, 1, 1,
                 1, 1, 1, 1, 1, 1, 1, 1
